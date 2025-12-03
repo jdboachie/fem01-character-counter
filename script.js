70257912233f53e66f42b639d4215d3ca54240b5
Jude Boachie <jude.boachie@amalitech.com>
2025-12-03 17:01:00 +0000

    fix: address potential DivisionByZero error

--- script.js
+++ script.js
@@ -154,6 +154,10 @@ let counter = {
   getLetterDensities(text) {
     const counts = this.getCharacterCounts(text);
     const totalLetters = Object.values(counts).reduce((a, b) => a + b, 0);
+    if (totalLetters === 0) {
+      return;
+    }
+
     const densities = Object.entries(counts)
       .map(([letter, count]) => ({
         letter,
